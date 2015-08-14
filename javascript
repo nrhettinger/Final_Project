4823771db79d90a9925d17c2bbd5846e94f9c541
N <nrhettinger@gmail.com>
2015-08-14 12:27:33 -0400

    test commit

--- javascript
+++
@@ -1,6 +0,0 @@
-
-
-$('#navMobile').click( function() {
-	$('nav').toggle();
-	}
-})
--- javascript.js
+++ javascript.js
@@ -1,6 +1,23 @@
+//javaScript for the index page
+
+$("#menu").click(function(){
+	$('#navList').toggle();
+})
 
 
-$('#navMobile').click( function() {
-	$('nav').toggle();
+
+
+
+
+//javaScript for the form on the contact page
+
+$("input").click(function(){
+	$(this).css({"border": "solid", "border-width": "3px", "border-color": "#66FF99"});
+	if ( $("input").not( (this) ) ) {
+		$("input").(this).css({"border-width": "1px", "border-color": "black"});
 	}
 })
+
+
+
+
